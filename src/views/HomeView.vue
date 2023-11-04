5914963b28f14f8e74f3da4adc2313b752c1d0d2
pavleerg <pavleergovic@gmail.com>
2023-11-04 13:26:37 +0100

    naiveUI init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    init
-  </div>
+    <NButton>Button 1</NButton>
+    <NButton>Button 2</NButton>
 </template>
 
 <script>
-export default {
+import { NButton } from "naive-ui";
 
-}
+export default {
+  components: {
+    NButton,
+  }
+};
 </script>
 
 <style>
-
 </style>
