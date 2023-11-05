b2bead6249ff55833cbb0c730ec47ef40bc2cb58
ianba <ian.balen6@gmail.com>
2023-11-05 08:59:38 +0100

    jebiga

--- src/services/WebSocketAPI.js
+++ src/services/WebSocketAPI.js
@@ -3,8 +3,8 @@ import { Client } from '@stomp/stompjs'
 import SockJS from 'sockjs-client/dist/sockjs.min.js'
 
 // WebSocket server endpoint
-const serverUrl = "https://codebooq-back-fa7f9a51e2d2.herokuapp.com/ws";
-//const serverUrl = "http://localhost:8080/ws"
+//const serverUrl = "https://codebooq-back-fa7f9a51e2d2.herokuapp.com/ws";
+const serverUrl = "http://localhost:8080/ws"
 // Instantiate WebSocket connection and STOMP client
 const ws = new SockJS(serverUrl)
 const client = new Client({
@@ -12,17 +12,31 @@ const client = new Client({
   reconnectDelay: 5000
 })
 
+// ... existing imports and code
+
 const WebSocketAPI = {
-        connect(parkingSpotEventsCallback, priceCallback) {
-          client.onConnect = () => {
-            client.subscribe('/topic/parkingSpotEvents', parkingSpotEventsCallback);
-            client.subscribe('/topic/price', priceCallback);
-          };
-          client.activate();
-        },
-        disconnect() {
-          client.deactivate();
-        }
+  connect(parkingSpotEventsCallback, priceCallback) {
+    client.onConnect = () => {
+      console.log("Connected to WebSocket");
+      client.subscribe('/topic/parkingSpotEvents', parkingSpotEventsCallback);
+      client.subscribe('/topic/price', priceCallback);
+    };
+
+    client.onStompError = (error) => {
+      console.error('STOMP Error:', error);
+    };
+
+    client.onWebSocketClose = (event) => {
+      console.log('WebSocket Closed:', event);
+    };
+
+    client.activate();
+  },
+  disconnect() {
+    client.deactivate();
+  }
 };
-      
+
 export default WebSocketAPI;
+
+      
--- src/stores/webSocket.js
+++ src/stores/webSocket.js
@@ -1,34 +1,41 @@
 import WebSocketAPI from '../services/WebSocketAPI.js';
 
+// ... existing imports
+
 export const webSocketModule = {
-        state: {
-          parkingSpotEvents: [],
-          prices: [],
+  state: {
+    parkingSpotEvents: [],
+    prices: [],
+    isConnected: false,
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.parkingSpotEvents.push(event);
+    },
+    ADD_PRICE(state, price) {
+      state.prices.push(price);
+    },
+    SET_CONNECTION_STATUS(state, status) {
+      state.isConnected = status;
+    }
+  },
+  actions: {
+    connectWebSocket({ commit }) {
+      WebSocketAPI.connect(
+        message => {
+          console.log(message.body);
+          commit('ADD_EVENT', JSON.parse(message.body));
         },
-        mutations: {
-          ADD_EVENT(state, event) {
-            state.parkingSpotEvents.push(event);
-          },
-          ADD_PRICE(state, price) {
-            state.prices.push(price);
-          }
-        },
-        actions: {
-          connectWebSocket({ commit }) {
-            WebSocketAPI.connect(
-              message => {
-                console.log(message.body);
-                commit('ADD_EVENT', JSON.parse(message.body));
-              },
-              message => {
-                console.log(message.body);
-                commit('ADD_PRICE', JSON.parse(message.body));
-              }
-            );
-          },
-          disconnectWebSocket() {
-            WebSocketAPI.disconnect();
-          }
+        message => {
+          console.log(message.body);
+          commit('ADD_PRICE', JSON.parse(message.body));
         }
-      };
-      
+      );
+      commit('SET_CONNECTION_STATUS', true);
+    },
+    disconnectWebSocket({ commit }) {
+      WebSocketAPI.disconnect();
+      commit('SET_CONNECTION_STATUS', false);
+    }
+  }
+};
