277a3c61b4f893f92f414fa3a3200a38921f52fa
pavleerg <pavleergovic@gmail.com>
2023-11-05 10:26:02 +0100

    removed dependancies

--- src/services/WebSocketAPI.js
+++ src/services/WebSocketAPI.js
@@ -1,12 +1,11 @@
 // src/services/WebSocketAPI.js
 import { Client } from '@stomp/stompjs'
-import SockJS from 'sockjs-client/dist/sockjs.min.js'
 
 // WebSocket server endpoint
 //const serverUrl = "https://codebooq-back-fa7f9a51e2d2.herokuapp.com/ws";
-const serverUrl = "http://localhost:8080/ws"
+//const serverUrl = "http://localhost:8080/ws"
 // Instantiate WebSocket connection and STOMP client
-const ws = new SockJS(serverUrl)
+const ws = "new SockJS(serverUrl)"
 const client = new Client({
   webSocketFactory: () => ws,
   reconnectDelay: 5000
